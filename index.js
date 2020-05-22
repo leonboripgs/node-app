0dde90bc6b4a14bbeffd979e3e780fb4c1fd35fa
Maxsim <leonboripgs0818@outlook.com>
2020-05-23 05:48:37 +1000

    new push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 // Respond with "hello world" for requests that hit our root "/"
 app.get("/", function (req, res) {
-    return res.send("Hello New World Again Again");
+    return res.send("Hello New World");
 });
 
 // listen to port 7000 by default
